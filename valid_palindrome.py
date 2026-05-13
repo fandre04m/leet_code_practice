34e7c1bfc19074597c8d547f5ebdcfa0fe07cad3
Fabio Andre Martins Coelho <fandre-m@c1r8s7.42porto.com>
2026-05-13 16:07:05 +0100

    Add working palindrome exercise

---
+++ valid_palindrome.py
@@ -0,0 +1,32 @@
+# Given a string s, return true if it is a palindrome, otherwise return false.
+#
+# A palindrome is a string that reads the same forward and backward.
+# It is also case-insensitive and ignores all non-alphanumeric characters.
+#
+# Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9)
+
+
+def is_palindrome_brute(s: str) -> bool:
+    clean_str = ""
+    for ch in s:
+        if ch.isalpha() or ch.isdigit():
+            clean_str += ch.lower()
+    return clean_str == clean_str[::-1]
+
+
+def is_palindrome_pointers(s: str) -> bool:
+    b = 0
+    e = len(s) - 1
+
+    while b < e:
+        while b < e and (not s[b].isalpha() or not s[b].isdigit()):
+            b += 1
+        while e > b and (not s[e].isalpha() or not s[e].isdigit()):
+            e -= 1
+        if s[b].lower() != s[e].lower():
+            return False
+    return True
+
+
+print(f"Using brute force: {is_palindrome_brute('girafarig?')}")
+print(f"Using pointers: {is_palindrome_pointers('Girafa rig?')}")
